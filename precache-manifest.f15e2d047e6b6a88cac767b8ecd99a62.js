fc647c0759aa0633c2c96e0d2e287f36a59e2cf8
datalater <the7mincheol@gmail.com>
2020-08-10 00:26:59 +0900

    Updates

---
+++ precache-manifest.f15e2d047e6b6a88cac767b8ecd99a62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb81674d4a5bd485c2fe6eda6e851be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58a61b8392a0c7dbbc6e",
+    "url": "/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58a61b8392a0c7dbbc6e",
+    "url": "/static/js/main.2b66fdee.chunk.js"
+  },
+  {
+    "revision": "31de383a84f0b531942d",
+    "url": "/static/js/runtime-main.408e7f6b.js"
+  }
+]);
